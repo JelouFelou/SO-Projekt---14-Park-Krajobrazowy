9a5dd1585f3f1ec395303c83298923f7afb1f0a6
JelouFelou <adr-bober@o2.pl>
2025-01-20 18:19:46 +0100

    Praca nad turysta.c

--- main.c
+++ main.c
@@ -5,22 +5,25 @@
 #include "header.h"
 
 sem_t wejscie_do_parku; // Semafor ograniczający liczbę turystów w parku
-sem_t przydzial_kasy;
 int wolna_kasa[K];
 
 int main() {
-	pthread_t kasjer_thread, przewodnik_thread, turysta_thread[N];
+	pthread_t /*kasjer_thread, przewodnik_thread,*/ turysta_thread[N];
 	srand(time(NULL));
 	
 	sem_init(&wejscie_do_parku,0,LIMIT);
 	
-	printf("Testowa symulacja wchodzenia turystów do parku\n");
+	for(int j=0; j<K; j++){
+		wolna_kasa[j]=1;
+	}
+	
+	printf("Testowa symulacja wchodzenia turystów do parku\n\n");
 	
 	// tworzenie turystów
 	for (int i = 1; i < (N+1); i++) {
 		sem_wait(&wejscie_do_parku);
 		pthread_create(&turysta_thread[i], NULL, turysta, i);
-		usleep((rand() % 1000 + 500) * 1000);
+		usleep((rand() % 10000 + 500) * 1000);
 	}
 	
 	// dołączanie wątków turystów
@@ -29,6 +32,6 @@ int main() {
 	}
 	
 	sem_destroy(&wejscie_do_parku);
-	printf("Poprawne zakończenie symulacji\n");
+	printf("\n\nPoprawne zakończenie symulacji\n");
 	return 0;
 }
--- turysta.c
+++ turysta.c
@@ -7,40 +7,60 @@
 
 // Deklaracja zmiennych globalnych
 extern int wolna_kasa[K];		// Stan kas
-extern sem_t przydzial_kasy; 	// Semafor obsługi kas
+extern sem_t wejscie_do_parku;
+//pthread_mutex_t przydzial_kasy = PTHREAD_MUTEX_INITIALIZER;
 
 void *turysta(void *arg) {
 	int id = (int)(long)arg;
-	int czas_pobytu = (rand() % 5) + 1;
+	int czas_pobytu = (rand() % 200) + 1;
 	int kupiono_bilet = 0;
+	int typ_turysty = (rand() % 100) + 1;
+	int typ_trasy = (rand() % 2) + 1;
+	int vip = 0;
+
+	printf(">>Turysta %d: Wchodzi do parku...\n", id);
 	
-	printf("Turysta %d: Wchodzi do parku...\n", id);
-	sleep(2);
-	
-	while(!kupiono_bilet){
-		sem_wait(&przydzial_kasy);
+	if(typ_turysty == 47){
+		printf("***Turysta %d jest VIP'em!***\n", id);
+		vip++;
+	}
 		
-		for(int i=0;i<K;i++){
-			if(wolna_kasa[i]){
-				wolna_kasa[i]=0;
-				printf("Turysta %d: Podchodzi do wolnej kasy %d...\n",id, i + 1);
-				sleep(2);
-				printf("Turysta %d: Kupił bilet w kasie %d...\n",id, i + 1);
-				kupiono_bilet = 1;
-				wolna_kasa[i] = 1;
-				break;
+	if(!vip){ // 
+		sleep((rand() % 15) + 1);
+		while(!kupiono_bilet){
+			for(int i=0;i<K;i++){
+				if(wolna_kasa[i]){
+					wolna_kasa[i]=0;
+					printf("Turysta %d: Podchodzi do wolnej kasy nr. %d...\n",id, i + 1);
+					sleep(2);
+					printf("Turysta %d: Zastanawia się nad wyborem...\n",id, i + 1);
+					
+					switch (typ_trasy){
+						case 1:
+							printf("$$1$$Turysta %d: Kupił bilet Trasy 1...\n",id);
+							break;
+						case 2:
+							printf("$$2$$Turysta %d: Kupił bilet Trasy 2...\n",id);
+							break;
+						default:
+							printf("!!!!!Turysta %d: Nie kupił biletu i zdecydował wyjść z parku...\n",id);
+							sem_post(&wejscie_do_parku);
+							pthread_exit(NULL);
+							break;
+					}
+					kupiono_bilet = 1;
+					wolna_kasa[i] = 1;
+					break;
+				}
 			}
-		}
+		}	
+	}else{
+		printf("***Turysta %d: Jako że jest VIP'em posiada legitymację PTTK***\n", id);
 		
-		sem_post(&przydzial_kasy);
-		
-		if(!kupiono_bilet){
-			printf("Turysta %d: Nie widzi wolnej kasy zatem czeka...\n", id);
-			sleep(2);
-		}
-	}		
-	printf("Turysta %d: Zwiedzał park przez %d s\n",id, czas_pobytu);
+	}
+	
 	sleep(czas_pobytu);
-	printf("Turysta %d: Kończy wizytę w parku \n", id);
+	printf("-----Turysta %d: Kończy wizytę w parku, który zwiedzał przez %d s-----\n", id, czas_pobytu);
+	sem_post(&wejscie_do_parku);
 	pthread_exit(NULL);
 }
